4dfd00173e0989d99a4a3c8fc8079179fd44b1af
MarinaPaley <marina_paley@mail.ru>
2021-03-03 19:54:20 +0300

    Добавлена документация кода #4

--- Task 1-3/Task 1-3/main.cpp
+++ Task 1-3/Task 1-3/main.cpp
@@ -18,12 +18,25 @@ int main()
     return 0;
 }
 
+
+/**
+ * \brief Метод перевода массы в граммах в массу в килограммах
+ * \param mass Масса в г
+ * \return Масса в кг
+ */
 double ConvertGramsToKgs(const double mass)
 {
     const auto kiloFactor = 1000;
     return mass * kiloFactor;
 }
 
+/**
+ *   \brief Метод расчёта кинетической энергии в СИ.
+ *
+ *   \param mass Масса в килограммах (кг).
+ *   \param velocity Скорость в метрах в секунду (м/с).
+ *   \return Значение кинетической энергии в Джоулях (Дж).
+ */
 double GetKineticEnergy(const double mass, const double velocity)
 {
     return mass * pow(velocity, 2) / 2;
